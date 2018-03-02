4fa17d86aa64f38b4e38ffc4b73da54a6aa7acb7
Kevin McCarthy <kevinpatrickmccarthy@gmail.com>
2018-03-02 21:49:27 +0000

    passing for two orders

--- snack-shack/snackShack.js
+++ snack-shack/snackShack.js
@@ -9,19 +9,32 @@ class snackShack {
   }
 
   getSchedule() {
-    return this.firstLine() + this.finalLine()
+    return this.firstLine() + this.additionalLines() + this.finalLine()
   }
 
-  firstLine(){
-    return '1. 0:00 ' + this.numberOfOrders + ' sandwich orders placed,' + ' start making sandwich 1\n2. 0:60 serve sandwich 1\n' 
+  firstLine() {
+    return '1. 0:00 ' + this.numberOfOrders + ' sandwich orders placed,' + ' start making sandwich 1\n2. 0:60 serve sandwich 1'  
   }
   
   additionalLines(){
-    return '\n3. 1:30 start making sandwich 2\n4. 2:30 serve sandwich 2\n'
+    if(this.numberOfOrders <= 1) return '\n'
+    let returnLines = ''
+    let currentTime = 90
+    for (var i = 1; i < this.numberOfOrders; i++) {
+      returnLines += '\n' + (i + 2).toString() + '. ' + snackShack.turnSecondsToMinutesAndSeconds(currentTime) + ' start making sandwich ' + (i + 1).toString() + '\n' + (i + 3).toString() + '. ' + snackShack.turnSecondsToMinutesAndSeconds(currentTime + 60) + ' serve sandwich ' + (i + 1).toString() 
+    }
+    return returnLines += '\n'
   } 
 	
   finalLine(){
-    return '3. 1:30 take a well earned break!'
+    return ((this.numberOfOrders * 2) + 1).toString() + '. ' + snackShack.turnSecondsToMinutesAndSeconds(this.numberOfOrders * 90) + ' take a well earned break!'
+  }
+
+  static turnSecondsToMinutesAndSeconds(totalSeconds) {
+    let minutes = Math.floor(totalSeconds / 60).toString()  
+    let seconds = (totalSeconds % 60).toString()
+    if (seconds.length === 1) seconds = '0' + seconds
+    return minutes + ':' + seconds
   }
 }
 
--- snack-shack/snackShack.test.js
+++ snack-shack/snackShack.test.js
@@ -14,7 +14,17 @@ test('it returns the correct schedule when two orders are placed', () => {
   shack.placeOrder()
   shack.placeOrder()
   
-  expect(shack.getSchedule()).toBe('1. 0:00 2 sandwich orders placed, start making sandwich 1\n2. 0:60 serve sandwich 1\n3. 1:30 start make sandwich 2\n4. 2:30 serve sandwich 2\n5. 3:00 take a well earned break!')
+  expect(shack.getSchedule()).toBe('1. 0:00 2 sandwich orders placed, start making sandwich 1\n2. 0:60 serve sandwich 1\n3. 1:30 start making sandwich 2\n4. 2:30 serve sandwich 2\n5. 3:00 take a well earned break!')
 })
 
+test('turnSecondsToMinutesAndSeconds when given 90 returns 1:30', () => {
+  expect(snackShack.turnSecondsToMinutesAndSeconds(90)).toBe('1:30')
+})
 
+test('turnsecondstominutesandseconds when given 250 returns 4:10', () => {
+  expect(snackShack.turnSecondsToMinutesAndSeconds(250)).toBe('4:10')
+})
+
+test('turnsecondstominutesandseconds when given 240 returns 4:00', () => {
+  expect(snackShack.turnSecondsToMinutesAndSeconds(240)).toBe('4:00')
+})
